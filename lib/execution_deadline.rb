c8f856f0bc3ccda6bb97493d5d4500f062060c67
Brian Malinconico <bmalinconico@terminus.com>
2019-09-28 14:14:07 -0400

    Inital commit

---
+++ lib/execution_deadline.rb
@@ -0,0 +1,6 @@
+require "execution_deadline/version"
+
+module ExecutionDeadline
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/execution_deadline/version.rb
@@ -0,0 +1,3 @@
+module ExecutionDeadline
+  VERSION = "0.1.0"
+end
